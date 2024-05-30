15ce729f7f52b1af13a24dcebf30518231598e7a
Tran, Tho <Tho.Tran@netapp.com>
2024-05-29 20:14:22 -0400

    reducing docs to 300

--- ollama/main.py
+++ ollama/main.py
@@ -10,7 +10,7 @@ ollama = Ollama(
     base_url='http://localhost:11434',
     model="llama3"
 )
-print(ollama.invoke("why is the sky blue"))
+# print(ollama.invoke("why is the sky blue"))
 
 loader = WebBaseLoader("https://www.gutenberg.org/files/1727/1727-h/1727-h.htm")
 data = loader.load()
@@ -18,12 +18,12 @@ text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
 all_splits = text_splitter.split_documents(data)
 
 # need to do ollama pull nomic-embed-text and pip install chromadb
-oembed = OllamaEmbeddings(base_url="http://localhost:11434", model="nomic-embed-text")
-vectorstore = Chroma.from_documents(documents=all_splits[:100], embedding=oembed)
+oembed = OllamaEmbeddings(base_url="http://localhost:11434", model="llama3")
+vectorstore = Chroma.from_documents(documents=all_splits[:300], embedding=oembed)
 
 question = "Who is Neleus and who is in Neleus' family?"
 docs = vectorstore.similarity_search(question)
-len(docs)
+print("docs", len(docs))
 
 qachain = RetrievalQA.from_chain_type(ollama, retriever=vectorstore.as_retriever())
-qachain.invoke({"query": question})
+print(qachain.invoke({"query": question}))
