6d7d0e48ca24cde3b3c889c44c6e441f574245f9
Tran, Tho <Tho.Tran@netapp.com>
2024-05-30 09:25:48 -0400

    improve speed by save processed text splits

--- ollama/README.md
+++ ollama/README.md
@@ -1,7 +1,12 @@
 ### Installation
-To bring up ollama container
-```commandline
-cd ollama
-docker-compose up -d
-```
+1) Bring up ollama container
+   ```commandline
+    cd ollama
+    docker-compose up -d
+    ```
+2) Pull all the required LLM models in ollama: llama3, nomic-embed-text
+3) Run the main.py script to start the chat
+    ```commandline
+    python main.py
+    ```
 
--- ollama/main.py
+++ ollama/main.py
@@ -4,21 +4,27 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import OllamaEmbeddings
 from langchain_community.vectorstores import Chroma
 from langchain.chains import RetrievalQA
+import pickle
 
 # need to do ollama pull llama3
 ollama = Ollama(
     base_url='http://localhost:11434',
     model="llama3"
 )
-# print(ollama.invoke("why is the sky blue"))
 
-# loader = WebBaseLoader("https://www.gutenberg.org/files/1727/1727-h/1727-h.htm")
-loader = DirectoryLoader("../HPP_data", "*.txt", use_multithreading=True, show_progress=True)
-data = loader.load()
-print("Number of docs: ", len(data))
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
-all_splits = text_splitter.split_documents(data)
-print("Number of splits: ", len(all_splits))
+try:
+    with open("text_splits.pkl", "rb") as f:
+        all_splits = pickle.load(f)
+except FileNotFoundError:
+    print("Loading raw text from disk")
+    loader = DirectoryLoader("../HPP_data", "*.txt", use_multithreading=True, show_progress=True)
+    data = loader.load()
+    print("Number of docs: ", len(data))
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
+    all_splits = text_splitter.split_documents(data)
+    print("Number of splits: ", len(all_splits))
+    with open("text_splits.pkl", "wb") as f:
+        pickle.dump(all_splits, f)
 # need to do ollama pull nomic-embed-text and pip install chromadb
 oembed = OllamaEmbeddings(base_url="http://localhost:11434", model="nomic-embed-text")
 vectorstore = Chroma.from_documents(documents=all_splits[:300], embedding=oembed)
@@ -33,4 +39,5 @@ while True:
     question = input("Ask a question: ")
     if question == "exit":
         break
-    print(qachain.invoke({"query": question}))
+    response = qachain.invoke({"query": question})
+    print("Answer: ", response["result"])
